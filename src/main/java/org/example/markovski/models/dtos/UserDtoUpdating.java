c9888e2ef3a4c28bf8af46685c5e38f808605b45
Kaloyan <kaloyan.enchev@abv.bg>
2024-04-04 12:36:41 +0300

    Added UserDtoUpdating

---
+++ src/main/java/org/example/markovski/models/dtos/UserDtoUpdating.java
@@ -0,0 +1,21 @@
+package org.example.markovski.models.dtos;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDtoUpdating {
+    @NotEmpty(message = "First name can't be empty")
+    @Size(min = 2, max = 20, message = "The first name must be between 2 symbols and 20 symbols.")
+    private String firstName;
+    @NotEmpty(message = "Last name can't be empty")
+    @Size(min = 2, max = 20, message = "The last name must be between 2 symbols and 20 symbols.")
+    private String lastName;
+    @NotEmpty(message = "Password can't be empty")
+    private String password;
+    @NotEmpty(message = "Phone number can't be empty")
+    private String phoneNumber;
+}
