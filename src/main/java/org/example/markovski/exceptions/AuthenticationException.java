9ce9a76f993c3448f40b0102725bd67496fc1b3a
Kaloyan <kaloyan.enchev@abv.bg>
2024-04-04 12:33:33 +0300

    Added AuthenticationException

---
+++ src/main/java/org/example/markovski/exceptions/AuthenticationException.java
@@ -0,0 +1,7 @@
+package org.example.markovski.exceptions;
+
+public class AuthenticationException extends RuntimeException{
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
