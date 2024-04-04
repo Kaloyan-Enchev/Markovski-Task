3aeee5fcae4f64b494d7309427d27c97ded78b0e
Kaloyan <kaloyan.enchev@abv.bg>
2024-04-04 12:41:25 +0300

    Added main class

---
+++ src/main/java/org/example/markovski/MarkovskiApplication.java
@@ -0,0 +1,13 @@
+package org.example.markovski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovskiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkovskiApplication.class, args);
+    }
+
+}
