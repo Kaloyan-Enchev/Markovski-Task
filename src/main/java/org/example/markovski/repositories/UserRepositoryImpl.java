d0ec0f0a0682a3c4c09c42e18d01ff75ae992d0b
Kaloyan <kaloyan.enchev@abv.bg>
2024-04-04 12:31:52 +0300

    Added UserRepositoryImpl

---
+++ src/main/java/org/example/markovski/repositories/UserRepositoryImpl.java
@@ -0,0 +1,146 @@
+package org.example.markovski.repositories;
+
+import org.example.markovski.exceptions.EntityNotFoundException;
+import org.example.markovski.helpers.filters.FilterOptionsUsers;
+import org.example.markovski.models.User;
+import org.example.markovski.repositories.contracts.UserRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public List<User> getAll(FilterOptionsUsers filterOptions) {
+        try (Session session = sessionFactory.openSession()) {
+            List<String> filters = new ArrayList<>();
+            Map<String, Object> params = new HashMap<>();
+
+            filterOptions.getFirstName().ifPresent(value -> {
+                filters.add("firstName like :firstName");
+                params.put("firstName", String.format("%%%s%%", value));
+            });
+
+            filterOptions.getLastName().ifPresent(value -> {
+                filters.add("lastName like :lastName");
+                params.put("lastName", String.format("%%%s%%", value));
+            });
+
+            filterOptions.getEmail().ifPresent(value -> {
+                filters.add("email like :email");
+                params.put("email", String.format("%%%s%%", value));
+            });
+
+            StringBuilder queryString = new StringBuilder("from User");
+            if (!filters.isEmpty()) {
+                queryString.append(" where ")
+                        .append(String.join(" and ", filters));
+            }
+            queryString.append(generateOrderBy(filterOptions));
+
+            Query<User> query = session.createQuery(queryString.toString(), User.class);
+            query.setProperties(params);
+            return query.list();
+        }
+    }
+
+    @Override
+    public User getById(int id) {
+        try (Session session = sessionFactory.openSession()) {
+            User user = session.get(User.class, id);
+            if (user == null) {
+                throw new EntityNotFoundException("User", id);
+            }
+            return user;
+        }
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<User> query = session.createQuery(
+                    "from User where email = :email", User.class);
+            query.setParameter("email", email);
+
+            List<User> result = query.list();
+            if (result.isEmpty()) {
+                throw new EntityNotFoundException("User", "email", email);
+            }
+
+            return result.get(0);
+        }
+    }
+
+    @Override
+    public void create(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(user);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void deleteUser(int id) {
+        User userToDelete = getById(id);
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.remove(userToDelete);
+            session.getTransaction().commit();
+        }
+    }
+
+    private String generateOrderBy(FilterOptionsUsers filterOptions) {
+        if (filterOptions.getSortBy().isEmpty()) {
+            return "";
+        }
+
+        String orderBy = "";
+        switch (filterOptions.getSortBy().get()) {
+            case "firstName":
+                orderBy = "firstName";
+                break;
+            case "lastName":
+                orderBy = "lastName";
+                break;
+            case "email":
+                orderBy = "email";
+                break;
+        }
+
+        if (!orderBy.isEmpty()) {
+            orderBy = " ORDER BY " + orderBy;
+
+            // Append sorting order if specified
+            if (filterOptions.getSortOrder().isPresent() &&
+                    filterOptions.getSortOrder().get().equalsIgnoreCase("desc")) {
+                orderBy += " DESC";
+            }
+        }
+
+        return orderBy;
+    }
+}
