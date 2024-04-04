89283c609026374f572ad9c60cf764ce271aee58
Kaloyan <kaloyan.enchev@abv.bg>
2024-04-04 12:44:07 +0300

    Added app properties

---
+++ src/test/java/org/example/markovski/MarkovskiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.markovski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkovskiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
